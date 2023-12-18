716ed3a3fd21702d4cfe89a111898dc2fe090abf
Margarita827 <margaiba5@gmail.com>
2023-12-18 11:10:01 +0100

    Select element

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function showColourHeart(event) {
+  if (event.target.value == "blue") {
+    alert("💙");
+  }
+  if (event.target.value == "red") {
+    alert("❤");
+  }
+  if (event.target.value == "yellow") {
+    alert("💛");
+  }
+}
 
+let colourSelect = document.querySelector("#colours");
+colourSelect.addEventListener("change", showColourHeart);
